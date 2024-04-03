9bc6470fc393f711f5f8ba3c88d955382310f59f
Falom Hernandez Ruiz <falom@MacBook-Pro-de-Falom.local>
2024-04-02 23:45:25 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-baa5133f-7c77-4a39-a3ce-3c765cb8df5a.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
